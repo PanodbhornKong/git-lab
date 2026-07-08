5683c0407e537700692735f03990560421874e22
PanodbhornKong <panodphorn@gmail.com>
2026-07-08 16:31:53 +0700

    Merge V2 and V3

--- calculate.py
+++ calculate.py
@@ -1,16 +1,12 @@
 def add(a, b):
     return a + b
 
-<<<<<<< HEAD
 def subtract(a, b):
     return a - b        
 
-print(add(2, 3))
-print(subtract(9, 4))
-=======
 def multiply(a, b):
     return a * b
 
 print(add(2, 3))
 print(multiply(4, 5))
->>>>>>> version3
+print(subtract(9, 4))
